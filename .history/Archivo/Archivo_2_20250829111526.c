188166ba889a0cc422c222800d43433a6524ab07
jcruz23 <Jcruz23.js@gmail.com>
2025-08-29 11:16:52 -0300

    29/08

---
+++ .history/Archivo/Archivo_2_20250829111526.c
@@ -0,0 +1,55 @@
+/*Escriba un programa que cree un menú de opciones que permita 
+a) abrir un archivo, b) escribir un archivo, c) leer un archivo. (Sin utilizar funciones)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *a; 
+    int op = 0;
+    char fin = ' ';
+    char ch;
+    char linea[100];
+    do{
+        system("clear");
+        printf("1)Abrir archivo \n2)Escribir archivo \n2)leer archivo\nOpcion: ");
+        scanf(" %d", &op);
+
+        switch(op){
+            case 1: 
+            a = fopen("archivo2.txt", "w");
+            printf("\nAbriendo archivo. . .");
+            if(a == NULL){
+                printf("\nError al abrir archivo");
+            }else{
+                printf("\nArchivo abierto!");
+            }
+            break;
+            case 2:
+            printf("\n----------------Modo Escritura----------------\n");
+            getchar();
+            while((ch = fgetc(stdin)) != EOF){
+                if(ch != '$'){
+                fputc(ch, a);
+                }else{
+                    break;
+                }
+            }
+            printf("\aCerrando archivo. . .");
+            fclose(a);
+            break;
+            case 3: 
+            printf("\nAbriendo archivo para leer");
+            a = fopen("archivo2.txt", "r");
+            printf("\n-----------------Modo Lectura-----------------\n");
+            while(fgets(linea, 100, a) != NULL){
+                printf("%s", linea);
+            }
+            }
+
+        printf("\nDesa continuar? (s/n)");
+        scanf(" %c", &fin);
+
+        
+    }while(fin == 's'|| fin == 'S');
+}
--- Esctructuras/Estructuras_2.c
+++ Esctructuras/Estructuras_2.c
@@ -12,28 +12,106 @@ si es 0 (cero) significa que son iguales los string.
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 struct cancion{
     char nombre[20];
-    int duración;
+    float duración;
     int peso;
 };
+int agregar(struct cancion *c, int);
+void menu();
+void mostrar(struct cancion *c, int);
+void buscar(struct cancion *c, int);
+void mayus(char *);
 
 int main(){
-    struct cancion *canciones;
-    canciones = malloc(3*sizeof(struct cancion));
+    struct cancion *c = NULL;
+    srand(time(NULL));
+    c = malloc(3*sizeof(struct cancion));
 
-    if(canciones == NULL){
-        printf("Error al asignar memoria");
-        return 1;
+    if(c == NULL){
+        printf("\aError asignando memoria");
     }
+   menu(c);
+   free(c);
 
-    for(int i = 0; i < 3; i++){
-        printf("Ingrese datos de la cancion[%d]: ", i+1);
+}
+
+
+void menu(struct cancion *c){
+ int n = 0;
+ int op;
+ char fin = ' ';
+
+    do{
+        system("clear");
+        printf("\n1-Agregar cancion\n2-mostrar canciones\n3-Buscar cancion\nOpcion: ");
+        scanf(" %d", &op);
+
+        switch(op){
+            case 1: n = agregar(c, n); break;
+            case 2: mostrar(c, n); break;
+            case 3: buscar(c, n); break;
+            default: printf("\n\aERROR!!"); break;
+        }
+
+        printf("\nDesea continuar? (s/n): ");
+        scanf(" %c", &fin);
+    }while(fin == 's'|| fin == 'S');
+}
+int agregar(struct cancion *c, int n){
+    if(n < 3){
+        printf("\n------------Cancion[%d]------------", n+1);
         printf("\nNombre: ");
-        fgets(canciones[i].nombre,sizeof(canciones[i].nombre),stdin);
-        canciones[i].nombre[strcspn(canciones[i].nombre, "\n")] = '\0';
+        getchar();
+        fgets(c[n].nombre, sizeof(c[n].nombre), stdin);
+        c[n].nombre[strcspn(c[n].nombre, "\n")] = '\0';
+        mayus(c[n].nombre);
+        printf("\nDuracion(min.seg): ");
+        scanf(" %f", &c[n].duración);
+        printf("\nAgregando. . .");
+        c[n].peso = rand()%(3000-1000+1)+1000;
+        n++;
+    }else{
+        printf("\n\aMemoria llena!");
     }
+    return n;
+}
 
-    free(canciones);
+void mostrar(struct cancion *c, int n){
+    printf("\na");
+    for(int i = 0; i < n; i++){
+        printf("\n------------Cancion[%d]------------", i+1);
+        printf("\nNombre: %s", c[i].nombre);
+        printf("\nDuracion: %.2f", c[i].duración);
+        printf("\nPeso: %d kb", c[i].peso);
+    }
+}
+
+void buscar(struct cancion *c, int n){
+char aux[20];
+printf("\nBuscar cancion: ");
+getchar();
+fgets(aux, sizeof(aux), stdin);
+aux[strcspn(aux, "\n")] = '\0';
+mayus(aux);
+
+for(int i = 0; i < n; i++){
+    if(strcmp(aux, c[i].nombre) == 0){
+            printf("\n------------Cancion[%d]------------", i+1);
+            printf("\nNombre: %s", c[i].nombre);
+            printf("\nDuracion: %.2f", c[i].duración);
+            printf("\nPeso: %d kb", c[i].peso);
+        }else{
+            printf("\nCancion no encontrada");
+       }
+    }
+}
+
+void mayus(char *nombre){
+    int cant = sizeof(nombre);
+    for(int i = 0; i < cant; i++){
+        *(nombre + i) = toupper(*(nombre + i));
+    }
 }
